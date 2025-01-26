ac7d7577a9505372cbb10fbfcdf17fb4df3a95fa
Yannick-NQ <yninaq@gmail.com>
2025-01-26 18:28:48 -0500

    fix(reset): Se añadio la funcionalidad para el boton de reinicio

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { calculateScore, checkGuess, getHighScore, getScoreHistory, MAX_NUMBER, MIN_NUMBER, resetGame, saveHighScore, saveScoreHistory } from "./gameLogic.js";
+import { calculateScore, checkGuess, getHighScore, getScoreHistory, MAX_NUMBER, MIN_NUMBER, resetGame, restartGame, saveHighScore, saveScoreHistory } from "./gameLogic.js";
 import { clearInputField, toggleButtonState, updateTextContent } from "./ui.js";
 
 let attempts = 1;
@@ -11,6 +11,12 @@ function startGame() {
     attempts = 1;
     updateTextContent('h1', '¡Adivina el número secreto!');
     updateTextContent('p', `Indica un número del ${MIN_NUMBER} al ${MAX_NUMBER}`);
+}
+
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        handleGuess();
+    }
 }
 
 function updateScoreboard() {
@@ -55,6 +61,7 @@ function handleGuess() {
             updateTextContent('.texto__puntaje', `Puntaje más alto: ${highScore}`)
         }
         toggleButtonState('#restart', false);
+        toggleButtonState('#reset', false);
     } else {
         if (result === 'too high') {
             updateTextContent('p', 'El número secreto es menor');
@@ -69,6 +76,14 @@ function handleGuess() {
 }
 
 function handleReset() {
+    resetGame();
+    updateScoreboard();
+    clearInputField('#userValue');
+    toggleButtonState('#restart', true);
+    toggleButtonState('#reset', true);
+}
+
+function handleRestart() {
     startGame();
     clearInputField('#userValue');
     toggleButtonState('#restart', true);
@@ -78,6 +93,8 @@ function handleReset() {
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.container__boton').addEventListener('click', handleGuess);
-    document.querySelector('#restart').addEventListener('click', handleReset);
+    document.querySelector('#restart').addEventListener('click', handleRestart);
+    document.querySelector('#reset').addEventListener('click', handleReset);
+    document.querySelector('#userValue').addEventListener('keydown', handleKeyPress);
     startGame();
 })
